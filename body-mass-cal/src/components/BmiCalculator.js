be5500dd3b8668eb5d7ddf6184da662224e2cb23
chikechris <cchike2017@gmail.com>
2020-03-07 17:30:39 -0500

    changed cm to m

--- body-mass-cal/src/components/BmiCalculator.js
+++ body-mass-cal/src/components/BmiCalculator.js
@@ -2,34 +2,29 @@ import React, { useState, useEffect } from 'react';
 import FormInput from './FormInput';
 
 const BmiCalculator = () => {
-  const [heightUnit, setHeightUnit] = useState('cm');
+  const [heightUnit, setHeightUnit] = useState('m');
   const [weightUnit, setWeightUnit] = useState('kg');
   const [unit, setUnit] = useState('Metric');
   // const [count, setCount] = useState({
-  //  data:{     
-  //       heightCount: '0', 
-  //       inchesCount: '0', 
+  //  data:{
+  //       heightCount: '0',
+  //       inchesCount: '0',
   //       weightCount: '0'
   //     }
-  // }) 
+  // })
   const [count, setCount] = useState({
+    heightCount: '0',
+    inchesCount: '0',
+    weightCount: '0'
+  });
 
-      heightCount: '0',
-      inchesCount: '0',
-      weightCount: '0'
-   
-  }) 
+  // const { heightCount, inchesCount, weightCount   }= count.data
+  const { heightCount, inchesCount, weightCount } = count;
 
-  // const { heightCount, inchesCount, weightCount   }= count.data 
-  const { heightCount, inchesCount, weightCount } = count
-
-
-  useEffect(() => {
-  
-  }, [])
+  useEffect(() => {}, []);
 
   const onChangeInput = e => {
-    const {name, value} = e.target 
+    const { name, value } = e.target;
     // const {data} = count
     // setCount({
     //  data:{
@@ -37,15 +32,16 @@ const BmiCalculator = () => {
     //     [name]: value
     // }
     // })
-    setCount(prevState =>({
-      ...prevState, [name]: value
-    }))
+    setCount(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
   };
 
   const onSelectTag = e => {
     setUnit(e.target.value);
     if (e.target.value === 'Metric') {
-      setHeightUnit('cm');
+      setHeightUnit('m');
       setWeightUnit('kg');
     } else {
       setHeightUnit('ft');
@@ -54,17 +50,16 @@ const BmiCalculator = () => {
   };
 
   const resetData = e => {
-    e.preventDefault()
-    setUnit('Metric') 
+    e.preventDefault();
+    setUnit('Metric');
     setCount({
       heightCount: '0',
       inchesCount: '0',
       weightCount: '0'
-    }) 
-    setHeightUnit('cm');
+    });
+    setHeightUnit('m');
     setWeightUnit('kg');
-
-  }
+  };
   return (
     <>
       <div className='bmi-inpute'>
@@ -106,7 +101,7 @@ const BmiCalculator = () => {
             type='text'
             name='weightCount'
             title={`Weight (${weightUnit})`}
-            value={weightCount }
+            value={weightCount}
             onChange={onChangeInput}
           />
         </div>
