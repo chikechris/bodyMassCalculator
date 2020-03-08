a06ac62c526605fd00028fc76d552b54f41953bd
chikechris <cchike2017@gmail.com>
2020-03-07 19:38:46 -0500

    Updates

---
+++ precache-manifest.8cae649066ed78fc23621781e2b5a2fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621e778d4118f546beb7d6757a3cd39a",
+    "url": "/bodyMassCalculator/index.html"
+  },
+  {
+    "revision": "89ca54580717ce6909e1",
+    "url": "/bodyMassCalculator/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fd14aff13dee73930dad",
+    "url": "/bodyMassCalculator/static/css/main.88837caa.chunk.css"
+  },
+  {
+    "revision": "89ca54580717ce6909e1",
+    "url": "/bodyMassCalculator/static/js/2.3dfe6f63.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/bodyMassCalculator/static/js/2.3dfe6f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd14aff13dee73930dad",
+    "url": "/bodyMassCalculator/static/js/main.6d9aa795.chunk.js"
+  },
+  {
+    "revision": "ccec9f4448b63bf4907b",
+    "url": "/bodyMassCalculator/static/js/runtime-main.22218859.js"
+  }
+]);
